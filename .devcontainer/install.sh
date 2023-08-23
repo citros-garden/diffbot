309ce1871a8995a20a4d6e5cfdf3a2efb430efda
lulav <iftahnaf@gmail.com>
2023-08-23 11:28:26 +0300

    fixing install script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 WORKSPACE_DIR="/workspaces/diffbot/"
 
+export ROS_DISTRO="humble"
+source /opt/ros/${ROS_DISTRO}/setup.sh
+
 cd $WORKSPACE_DIR
-git submodule update --init --recursive
+
 vcs import src < src/ros2_control_demos/ros2_control_demos.$ROS_DISTRO.repos
 rosdep update --rosdistro=$ROS_DISTRO
 sudo apt-get update
 
 rosdep install --from-paths src --ignore-src -r -y
 
-echo "source /opt/ros/${ROS_DISTRO}/setup.sh" >> ~/.bashrc
-colcon build --symlink-install
 
-echo "source /workspaces/diffbot/install/setup.bash" >> ~/.bashrc
 
-sudo apt update
 git clone git@github.com:ros2/teleop_twist_keyboard.git
 cd teleop_twist_keyboard
 git submodule update --init --recursive
+
+colcon build --symlink-install
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.sh" >> ~/.bashrc
+echo "source /workspaces/diffbot/install/setup.bash" >> ~/.bashrc
 
 
 
