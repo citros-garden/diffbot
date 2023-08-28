0c37c94b61f248f5ea744b718efbf4d634d36fa6
orr_lulav.space <114152002+orrlulavspace@users.noreply.github.com>
2023-08-28 10:09:55 +0300

    Setup (#2)
    * fixing the simulation
    
    * fix_sim
    
    * fixing the simulation
    
    * fix readme
    
    * add submodule
    
    * fix submodule
    
    * fixing install script
    
    * fix submodule
    
    * fix submodule
    
    * fix submodule
    
    * fix submodule
    
    ---------
    
    Co-authored-by: lulav <iftahnaf@gmail.com>

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 WORKSPACE_DIR="/workspaces/diffbot/"
 
+export ROS_DISTRO="humble"
+source /opt/ros/${ROS_DISTRO}/setup.sh
+
 cd $WORKSPACE_DIR
 
-. /opt/ros/"${ROS_DISTRO}"/setup.sh
-. /home/ros2_ws/install/setup.sh
-exec "$@"
+vcs import src < src/ros2_control_demos/ros2_control_demos.$ROS_DISTRO.repos
+rosdep update --rosdistro=$ROS_DISTRO
+sudo apt-get update
 
-# colcon build --symlink-install
-# source install/setup.bash
+rosdep install --from-paths src --ignore-src -r -y
+
+git clone https://github.com/ros2/teleop_twist_keyboard.git
+cd teleop_twist_keyboard
+git submodule update --init --recursive
+
+cd $WORKSPACE_DIR
+colcon build --symlink-install
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.sh" >> ~/.bashrc
+echo "source /workspaces/diffbot/install/setup.bash" >> ~/.bashrc
 
 
-# rosdep install --from-paths src --ignore-src -r -y
 
 
 echo "
---
+++ src/ros2_control/doc/index.rst
@@ -0,0 +1,35 @@
+:github_url: https://github.com/ros-controls/ros2_control/blob/{REPOS_FILE_BRANCH}/doc/index.rst
+
+.. _ros2_control_framework:
+
+#################
+ros2_control
+#################
+
+`GitHub Repository <https://github.com/ros-controls/ros2_control>`_
+
+=================
+API Documentation
+=================
+
+API documentation is parsed by doxygen and can be found `here <../../api/index.html>`_
+
+
+
+=========
+Features
+=========
+
+* :ref:`Command Line Interface (CLI) <ros2controlcli_userdoc>`
+
+
+========
+Concepts
+========
+
+.. toctree::
+   :titlesonly:
+
+   Controller Manager <../controller_manager/doc/userdoc.rst>
+   Hardware Components <../hardware_interface/doc/hardware_components_userdoc.rst>
+   Mock Components <../hardware_interface/doc/mock_components_userdoc.rst>
